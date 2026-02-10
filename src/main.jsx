241fc03aadcda1789955dc6ccc1e430d346eea17
Benchetrit Binyamin <binou.ben26@gmail.com>
2026-02-10 07:21:58 +0200

    Fix: Désactiver Service Worker qui cause pages blanches

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
-import { registerServiceWorker } from './utils/registerServiceWorker';
+
+// Nettoyage des anciens Service Workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+  caches.keys().then(names => names.forEach(name => caches.delete(name)));
+}
 
 // Création de la racine React 18
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <App />
+    <App />
   </React.StrictMode>,
 )
-registerServiceWorker();
